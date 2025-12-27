9f8e6422fce4ca2b84307b2e8df7e9bbb34f9a8b
Somali-Post <Abdiazizga'al@moct.gov.so>
2025-12-27 09:25:26 +0300

    error fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import withPWA from "@ducanh2912/next-pwa";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  turbopack: {},
 };
 
 export default withPWA({
