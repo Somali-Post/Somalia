742b678826bc2e48fc27830dcdfbecf4072afb54
Somali-Post <Abdiazizga'al@moct.gov.so>
2025-12-31 11:03:38 +0300

    Arabic language fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,14 @@
-import { Inter } from "next/font/google";
+import { Inter, Noto_Sans_Arabic } from "next/font/google";
 import { cookies } from "next/headers";
 import "./globals.css";
 
 const inter = Inter({
   subsets: ["latin"],
+});
+
+const arabic = Noto_Sans_Arabic({
+  subsets: ["arabic"],
+  variable: "--font-arabic",
 });
 
 export const viewport = {
@@ -20,7 +25,11 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang={locale} dir={dir}>
-      <body className={`${inter.className} antialiased`}>
+      <body
+        className={`${
+          locale === "ar" ? arabic.className : inter.className
+        } antialiased`}
+      >
         {children}
       </body>
     </html>
